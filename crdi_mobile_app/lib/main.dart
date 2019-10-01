65db5562cbe594623c64a002ec2d7880c2049a31
Tyler Tichler <ttichle@siue.edu>
2019-10-01 15:52:36 -0500

    Fixed Git issues

--- crdi_mobile_app/lib/main.dart
+++ crdi_mobile_app/lib/main.dart
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Click Counter:',
+              'Times clicked:',
             ),
             Text(
               '$_counter',
