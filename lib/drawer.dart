913952cd6399198be0f7c752b7ed15ea854033de
Tyler Tichler <ttichle@siue.edu>
2019-11-06 14:13:12 -0600

    Added functionality to home button in bottombar so it redirects to the main menu of the app. I believe I've also added functionality to the back button as well, but that'll need to wait until the app has full navigation to test thoroughly

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
 
 class DrawerSection extends StatelessWidget with PreferredSizeWidget {
-  final Column things;
-  DrawerSection(this.things);
+  //final Column things;
+  DrawerSection();
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Container(color: Colors.blueGrey[600], child: things),
+      child: Container(
+        color: Colors.blueGrey[600],
+        /*child: things*/
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Vibration (False Value)'),
+                Switch(
+                  value: false,
+                  onChanged: null,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Theme (True Value)'),
+                Switch(
+                  value: true,
+                  onChanged: null,
+                )
+              ],
+            ),
+            FlatButton(
+              child: Text('Legal'),
+              onPressed: null,
+              textColor: Colors.white,
+            ),
+            FlatButton(
+              child: Text('How-to'),
+              onPressed: null,
+              textColor: Colors.white,
+            )
+          ],
+        ),
+      ),
     );
   }
 
