ae4d1d4bbf951ac16f95d2e4a16be4eb434ace6c
Cole Howard <zizofifo@tutanota.com>
2019-11-07 16:41:22 -0600

    How To text now placeholder; How To page uses bars and drawer

--- lib/how_to.dart
+++ lib/how_to.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'dart:async' show Future;
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:crdi_mobile_app/core/topbar.dart';
+import 'package:crdi_mobile_app/core/config.dart';
+import 'package:crdi_mobile_app/core/bottombar.dart';
 
 class HowToClass extends StatelessWidget {
-  Future<String> getStringOfHowToFile(BuildContext context) async {
+  Future<String> _getStringOfHowToFile(BuildContext context) async {
     return await DefaultAssetBundle.of(context)
         .loadString("assets/howtouse.md");
   }
@@ -12,16 +15,17 @@ class HowToClass extends StatelessWidget {
   @override
   Widget build(BuildContext buildContext) {
     return FutureBuilder(
-        future: getStringOfHowToFile(buildContext),
+        future: _getStringOfHowToFile(buildContext),
         builder: (BuildContext context, AsyncSnapshot<String> text) {
           return SafeArea(
             child: Scaffold(
-              appBar: AppBar(
-                title: const Text("How To"),
-              ),
+              appBar: TopAppBar("How To", true),
+              drawer: Drawer(),
+              bottomNavigationBar: BottomNavBar(false),
               body: Markdown(data: text.data),
             ),
           );
-        });
+        }
+    );
   }
 }
