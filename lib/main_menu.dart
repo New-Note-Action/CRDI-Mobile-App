745c95174a3323f6869978c4eb66436bc1aec87c
Cole Howard <zizofifo@tutanota.com>
2019-11-07 17:17:15 -0600

    Fix style of source code for Main Menu's build()

--- lib/main_menu.dart
+++ lib/main_menu.dart
@@ -28,21 +28,30 @@ class MainMenuClass extends StatelessWidget {
   Widget build(BuildContext buildContext) {
     _theContext = buildContext;
     return SafeArea(
-        child: Scaffold(
-            body: Column(children: <Widget>[
-      Container(
-        child: Image(
-            image: AssetImage("assets/Electronic-Component-Potentiometer.png"),
-            height: 200.0),
+      child: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Container(
+              child: Image(
+                  image: AssetImage(
+                      "assets/Electronic-Component-Potentiometer.png"
+                  ),
+                  height: 200.0
+              ),
+            ),
+            Expanded(
+              child: ListView.separated(
+                itemCount: _mainMenuItems.length,
+                separatorBuilder: (BuildContext context, int index) =>
+                    Divider(),
+                itemBuilder: (BuildContext context, int index) {
+                  return _mainMenuItems[index];
+                },
+              ),
+            ),
+          ],
+        ),
       ),
-      Expanded(
-          child: ListView.separated(
-        itemCount: _mainMenuItems.length,
-        separatorBuilder: (BuildContext context, int index) => Divider(),
-        itemBuilder: (BuildContext context, int index) {
-          return _mainMenuItems[index];
-        },
-      ))
-    ])));
+    );
   }
 }
