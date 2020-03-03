New-Note-Action/CRDI-Mobile-App

commit 3a23ab94050b3979d115ba3019be486bce71edbc

Move all profiles' BoxDecorations to new file 'profile_parts.dart'

lib/profiles/profile1.dart
@@ -4,6 +4,7 @@ import 'package:crdi_mobile_app/route_settings.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:crdi_mobile_app/inputs/button.dart';
 import 'package:crdi_mobile_app/inputs/slider.dart';
+import 'package:crdi_mobile_app/profiles/profile_parts.dart';
 
 class Profile1 extends StatelessWidget {
   @override
@@ -18,15 +19,7 @@ class Profile1 extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
-                decoration: const BoxDecoration(
-                  color: Color(0x00000000),
-                  border: Border(
-                    top: BorderSide(width: 1.0, color: Colors.white30),
-                    left: BorderSide(width: 1.0, color: Colors.white30),
-                    //right: BorderSide(width: 1.0, color: Colors.white30),
-                    bottom: BorderSide(width: 1.0, color: Colors.white30),
-                  ),
-                ),
+                decoration: profilePartNonControlDecoration,
                 width: ScreenUtil.screenWidthDp * 0.8,
                 height: ScreenUtil.screenHeightDp * 0.4,
                 child: Container(
@@ -41,15 +34,7 @@ class Profile1 extends StatelessWidget {
               Container(
                 width: ScreenUtil.screenWidthDp * 0.2,
                 height: ScreenUtil.screenHeightDp * 0.4,
-                decoration: const BoxDecoration(
-                  color: Color(0x00000000),
-                  border: Border(
-                    top: BorderSide(width: 1.0, color: Colors.white30),
-                    left: BorderSide(width: 1.0, color: Colors.white30),
-                    right: BorderSide(width: 1.0, color: Colors.white30),
-                    bottom: BorderSide(width: 1.0, color: Colors.white30),
-                  ),
-                ),
+                decoration: profilePartNonControlDecoration,
                 child: IconButton(
                   alignment: Alignment(0.0, 0.0),
                   icon: Icon(Icons.pause_circle_outline),
@@ -81,28 +66,12 @@ class Profile1 extends StatelessWidget {
                     Spacer(),
                   ],
                 ),
-                decoration: const BoxDecoration(
-                  color: Color(0x00000000),
-                  border: Border(
-                    //top: BorderSide(width: 1.0, color: Colors.black),
-                    left: BorderSide(width: 1.0, color: Colors.white30),
-                    right: BorderSide(width: 1.0, color: Colors.black),
-                    bottom: BorderSide(width: 1.0, color: Colors.black),
-                  ),
-                ),
+                decoration: profilePartControlDecoration,
               ),
               Container(
                 width: ScreenUtil.screenWidthDp * 0.334,
                 height: ScreenUtil.screenHeightDp * 0.6,
-                decoration: const BoxDecoration(
-                  color: Color(0x00000000),
-                  border: Border(
-                    //top: BorderSide(width: 1.0, color: Colors.black),
-                    left: BorderSide(width: 1.0, color: Colors.black),
-                    right: BorderSide(width: 1.0, color: Colors.black),
-                    bottom: BorderSide(width: 1.0, color: Colors.black),
-                  ),
-                ),
+                decoration: profilePartControlDecoration,
                 child: Container(
                   margin:
                       const EdgeInsets.only(top: 3.0, left: 10.0, right: 10.0),
@@ -152,15 +121,7 @@ class Profile1 extends StatelessWidget {
                   ],
                 ),
                 alignment: Alignment(0.0, 0.0),
-                decoration: const BoxDecoration(
-                  color: Color(0x00000000),
-                  border: Border(
-                    //top: BorderSide(width: 1.0, color: Colors.black),
-                    left: BorderSide(width: 1.0, color: Colors.black),
-                    right: BorderSide(width: 1.0, color: Colors.white30),
-                    bottom: BorderSide(width: 1.0, color: Colors.black),
-                  ),
-                ),
+                decoration: profilePartControlDecoration,
               ),
             ],
           ),

lib/profiles/profile2.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:crdi_mobile_app/route_settings.dart';
 import 'package:crdi_mobile_app/inputs/button.dart';
 import 'package:crdi_mobile_app/inputs/slider.dart';
+import 'package:crdi_mobile_app/profiles/profile_parts.dart';
 
 class Profile2 extends StatelessWidget {
   @override
@@ -15,15 +16,7 @@ class Profile2 extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.black87,
-                      border: Border(
-                        top: BorderSide(width: 1.0, color: Colors.white30),
-                        left: BorderSide(width: 1.0, color: Colors.white30),
-//right: BorderSide(width: 1.0, color: Colors.white30),
-                        bottom: BorderSide(width: 1.0, color: Colors.white30),
-                      ),
-                    ),
+                    decoration: profilePartNonControlDecoration,
                     width: MediaQuery.of(context).size.width * 0.8,
                     height: MediaQuery.of(context).size.height * 0.4,
                     child: Container(
@@ -38,15 +31,7 @@ class Profile2 extends StatelessWidget {
                   Container(
                     width: MediaQuery.of(context).size.width * 0.2,
                     height: MediaQuery.of(context).size.height * 0.4,
-                    decoration: const BoxDecoration(
-                      color: Colors.black87,
-                      border: Border(
-                        top: BorderSide(width: 1.0, color: Colors.white30),
-                        left: BorderSide(width: 1.0, color: Colors.white30),
-                        right: BorderSide(width: 1.0, color: Colors.white30),
-                        bottom: BorderSide(width: 1.0, color: Colors.white30),
-                      ),
-                    ),
+                    decoration: profilePartNonControlDecoration,
                     child: IconButton(
                       alignment: Alignment(0.0, 0.0),
                       icon: Icon(Icons.pause_circle_outline),
@@ -77,15 +62,7 @@ class Profile2 extends StatelessWidget {
                         Spacer(),
                       ],
                     ),
-                    decoration: const BoxDecoration(
-                      color: Colors.grey,
-                      border: Border(
-//top: BorderSide(width: 1.0, color: Colors.black),
-                        left: BorderSide(width: 1.0, color: Colors.white30),
-                        right: BorderSide(width: 1.0, color: Colors.black),
-                        bottom: BorderSide(width: 1.0, color: Colors.black),
-                      ),
-                    ),
+                    decoration: profilePartControlDecoration,
                   ),
                   Container(
                     width: MediaQuery.of(context).size.width * 0.333,
@@ -101,15 +78,7 @@ class Profile2 extends StatelessWidget {
                         Spacer(),
                       ],
                     ),
-                    decoration: const BoxDecoration(
-                      color: Colors.grey,
-                      border: Border(
-//top: BorderSide(width: 1.0, color: Colors.black),
-                        left: BorderSide(width: 1.0, color: Colors.white30),
-                        right: BorderSide(width: 1.0, color: Colors.black),
-                        bottom: BorderSide(width: 1.0, color: Colors.black),
-                      ),
-                    ),
+                    decoration: profilePartControlDecoration,
                   ),
                   Container(
                     width: MediaQuery.of(context).size.width * 0.333,
@@ -125,15 +94,7 @@ class Profile2 extends StatelessWidget {
                         ),
                       ],
                     ),
-                    decoration: const BoxDecoration(
-                      color: Colors.grey,
-                      border: Border(
-//top: BorderSide(width: 1.0, color: Colors.black),
-                        left: BorderSide(width: 1.0, color: Colors.white30),
-                        right: BorderSide(width: 1.0, color: Colors.black),
-                        bottom: BorderSide(width: 1.0, color: Colors.black),
-                      ),
-                    ),
+                    decoration: profilePartControlDecoration,
                   ),
                 ],
               ),

commit 6fd91ad447094837f6b28b9fddcc600597763f5d

Fix theming regressions introduced with 3a23ab9
BoxDecorations are now generated (mostly) automatically. Still less
lines of code per profile[0-9].dart file. Progress?

lib/profiles/profile1.dart
@@ -19,7 +19,9 @@ class Profile1 extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
-                decoration: profilePartNonControlDecoration,
+                decoration: makeNonControlDecoration(
+                  location: ProfileContainerLocation.leftmost,
+                ),
                 width: ScreenUtil.screenWidthDp * 0.8,
                 height: ScreenUtil.screenHeightDp * 0.4,
                 child: Container(
@@ -32,9 +34,11 @@ class Profile1 extends StatelessWidget {
                 ),
               ),
               Container(
+                decoration: makeNonControlDecoration(
+                  location: ProfileContainerLocation.rightmost,
+                ),
                 width: ScreenUtil.screenWidthDp * 0.2,
                 height: ScreenUtil.screenHeightDp * 0.4,
-                decoration: profilePartNonControlDecoration,
                 child: IconButton(
                   alignment: Alignment(0.0, 0.0),
                   icon: Icon(Icons.pause_circle_outline),
@@ -52,6 +56,9 @@ class Profile1 extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
+                decoration: makeControlDecoration(
+                  location: ProfileContainerLocation.leftmost,
+                ),
                 width: ScreenUtil.screenWidthDp * 0.333,
                 height: ScreenUtil.screenHeightDp * 0.6,
                 child: Column(
@@ -66,12 +73,13 @@ class Profile1 extends StatelessWidget {
                     Spacer(),
                   ],
                 ),
-                decoration: profilePartControlDecoration,
               ),
               Container(
+                decoration: makeControlDecoration(
+                  location: ProfileContainerLocation.inner,
+                ),
                 width: ScreenUtil.screenWidthDp * 0.334,
                 height: ScreenUtil.screenHeightDp * 0.6,
-                decoration: profilePartControlDecoration,
                 child: Container(
                   margin:
                       const EdgeInsets.only(top: 3.0, left: 10.0, right: 10.0),
@@ -104,6 +112,9 @@ class Profile1 extends StatelessWidget {
                 ),
               ),
               Container(
+                decoration: makeControlDecoration(
+                  location: ProfileContainerLocation.rightmost,
+                ),
                 width: ScreenUtil.screenWidthDp * 0.333,
                 height: ScreenUtil.screenHeightDp * 0.6,
                 child: Column(
@@ -121,7 +132,6 @@ class Profile1 extends StatelessWidget {
                   ],
                 ),
                 alignment: Alignment(0.0, 0.0),
-                decoration: profilePartControlDecoration,
               ),
             ],
           ),

lib/profiles/profile2.dart
@@ -16,7 +16,9 @@ class Profile2 extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    decoration: profilePartNonControlDecoration,
+                    decoration: makeNonControlDecoration(
+                      location: ProfileContainerLocation.leftmost,
+                    ),
                     width: MediaQuery.of(context).size.width * 0.8,
                     height: MediaQuery.of(context).size.height * 0.4,
                     child: Container(
@@ -29,9 +31,11 @@ class Profile2 extends StatelessWidget {
                     ),
                   ),
                   Container(
+                    decoration: makeNonControlDecoration(
+                      location: ProfileContainerLocation.rightmost,
+                    ),
                     width: MediaQuery.of(context).size.width * 0.2,
                     height: MediaQuery.of(context).size.height * 0.4,
-                    decoration: profilePartNonControlDecoration,
                     child: IconButton(
                       alignment: Alignment(0.0, 0.0),
                       icon: Icon(Icons.pause_circle_outline),
@@ -49,6 +53,9 @@ class Profile2 extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
+                    decoration: makeControlDecoration(
+                      location: ProfileContainerLocation.leftmost,
+                    ),
                     width: MediaQuery.of(context).size.width * 0.333,
                     height: MediaQuery.of(context).size.height * 0.6,
                     child: Column(
@@ -62,9 +69,11 @@ class Profile2 extends StatelessWidget {
                         Spacer(),
                       ],
                     ),
-                    decoration: profilePartControlDecoration,
                   ),
                   Container(
+                    decoration: makeControlDecoration(
+                      location: ProfileContainerLocation.inner,
+                    ),
                     width: MediaQuery.of(context).size.width * 0.333,
                     height: MediaQuery.of(context).size.height * 0.6,
                     child: Column(
@@ -78,9 +87,11 @@ class Profile2 extends StatelessWidget {
                         Spacer(),
                       ],
                     ),
-                    decoration: profilePartControlDecoration,
                   ),
                   Container(
+                    decoration: makeControlDecoration(
+                      location: ProfileContainerLocation.rightmost,
+                    ),
                     width: MediaQuery.of(context).size.width * 0.333,
                     height: MediaQuery.of(context).size.height * 0.6,
                     child: Column(
@@ -94,7 +105,6 @@ class Profile2 extends StatelessWidget {
                         ),
                       ],
                     ),
-                    decoration: profilePartControlDecoration,
                   ),
                 ],
               ),

lib/profiles/profile3.dart
@@ -23,7 +23,9 @@ class Profile3 extends StatelessWidget {
                 Expanded(
                   flex: 7,
                   child: Container(
-                    decoration: profilePartNonControlDecoration,
+                    decoration: makeNonControlDecoration(
+                      location: ProfileContainerLocation.leftmost,
+                    ),
                     child: Container(
                       margin: const EdgeInsets.all(10.0),
                       child: Text(
@@ -37,8 +39,10 @@ class Profile3 extends StatelessWidget {
                 Expanded(
                   flex: 3,
                   child: Container(
+                    decoration: makeNonControlDecoration(
+                      location: ProfileContainerLocation.rightmost,
+                    ),
                     height: double.infinity,
-                    decoration: profilePartNonControlDecoration,
                     child: IconButton(
                       alignment: Alignment(0.0, 0.0),
                       icon: Icon(Icons.pause_circle_outline),
@@ -61,6 +65,9 @@ class Profile3 extends StatelessWidget {
               children: [
                 Expanded(
                   child: Container(
+                    decoration: makeControlDecoration(
+                      location: ProfileContainerLocation.leftmost,
+                    ),
                     child: Column(
                       children: [
                         Spacer(),
@@ -73,12 +80,13 @@ class Profile3 extends StatelessWidget {
                         Spacer(),
                       ],
                     ),
-                    decoration: profilePartControlDecoration,
                   ),
                 ),
                 Expanded(
                   child: Container(
-                    decoration: profilePartControlDecoration,
+                    decoration: makeControlDecoration(
+                      location: ProfileContainerLocation.inner,
+                    ),
                     child: Container(
                       margin: const EdgeInsets.only(
                           top: 3.0, left: 10.0, right: 10.0),
@@ -91,7 +99,7 @@ class Profile3 extends StatelessWidget {
                             child: JoystickView(
                               iconsColor: Colors.white30,
                               backgroundColor: Colors.black54,
-                              innerCircleColor: Colors.black26,
+                              innerCircleColor: Color(0xFF222222),
                               size: MediaQuery.of(context).size.width * 0.26,
                             ),
                           ),
@@ -107,6 +115,9 @@ class Profile3 extends StatelessWidget {
                 ),
                 Expanded(
                   child: Container(
+                    decoration: makeControlDecoration(
+                      location: ProfileContainerLocation.rightmost,
+                    ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
@@ -122,7 +133,7 @@ class Profile3 extends StatelessWidget {
                       ],
                     ),
                     alignment: Alignment(0.0, 0.0),
-                    decoration: profilePartControlDecoration,
+
                   ),
                 )
               ],

lib/profiles/profile4.dart
@@ -14,7 +14,9 @@ class Profile4 extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
-                decoration: profilePartNonControlDecoration,
+                decoration: makeNonControlDecoration(
+                  location: ProfileContainerLocation.leftmost,
+                ),
                 width: MediaQuery.of(context).size.width * 0.8,
                 height: MediaQuery.of(context).size.height * 0.4,
                 child: Container(
@@ -27,9 +29,11 @@ class Profile4 extends StatelessWidget {
                 ),
               ),
               Container(
+                decoration: makeNonControlDecoration(
+                  location: ProfileContainerLocation.rightmost,
+                ),
                 width: MediaQuery.of(context).size.width * 0.2,
                 height: MediaQuery.of(context).size.height * 0.4,
-                decoration: profilePartNonControlDecoration,
                 child: IconButton(
                   alignment: Alignment(0.0, 0.0),
                   icon: Icon(Icons.pause_circle_outline),
@@ -47,14 +51,18 @@ class Profile4 extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
+                decoration: makeControlDecoration(
+                  location: ProfileContainerLocation.leftmost,
+                ),
                 width: MediaQuery.of(context).size.width * 0.333,
                 height: MediaQuery.of(context).size.height * 0.6,
-                decoration: profilePartControlDecoration,
               ),
               Container(
+                decoration: makeControlDecoration(
+                  location: ProfileContainerLocation.inner,
+                ),
                 width: MediaQuery.of(context).size.width * 0.334,
                 height: MediaQuery.of(context).size.height * 0.6,
-                decoration: profilePartControlDecoration,
                 child: Container(
                   margin:
                       const EdgeInsets.only(top: 3.0, left: 10.0, right: 10.0),
@@ -86,6 +94,9 @@ class Profile4 extends StatelessWidget {
                 ),
               ),
               Container(
+                decoration: makeControlDecoration(
+                  location: ProfileContainerLocation.rightmost,
+                ),
                 width: MediaQuery.of(context).size.width * 0.333,
                 height: MediaQuery.of(context).size.height * 0.6,
                 child: Column(
@@ -96,7 +107,6 @@ class Profile4 extends StatelessWidget {
                   ],
                 ),
                 alignment: Alignment(0.0, 0.0),
-                decoration: profilePartControlDecoration,
               ),
             ],
           ),

lib/profiles/profile5.dart
@@ -15,7 +15,9 @@ class Profile5 extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
-                decoration: profilePartNonControlDecoration,
+                decoration: makeNonControlDecoration(
+                  location: ProfileContainerLocation.leftmost,
+                ),
                 width: MediaQuery.of(context).size.width * 0.8,
                 height: MediaQuery.of(context).size.height * 0.4,
                 child: Container(
@@ -28,9 +30,11 @@ class Profile5 extends StatelessWidget {
                 ),
               ),
               Container(
+                decoration: makeNonControlDecoration(
+                  location: ProfileContainerLocation.rightmost,
+                ),
                 width: MediaQuery.of(context).size.width * 0.2,
                 height: MediaQuery.of(context).size.height * 0.4,
-                decoration: profilePartNonControlDecoration,
                 child: IconButton(
                   alignment: Alignment(0.0, 0.0),
                   icon: Icon(Icons.pause_circle_outline),
@@ -48,15 +52,19 @@ class Profile5 extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
+                decoration: makeControlDecoration(
+                  location: ProfileContainerLocation.leftmost,
+                ),
                 width: MediaQuery.of(context).size.width * 0.333,
                 height: MediaQuery.of(context).size.height * 0.6,
                 child: AddButton(),
-                decoration: profilePartControlDecoration,
               ),
               Container(
+                decoration: makeControlDecoration(
+                  location: ProfileContainerLocation.inner,
+                ),
                 width: MediaQuery.of(context).size.width * 0.334,
                 height: MediaQuery.of(context).size.height * 0.6,
-                decoration: profilePartControlDecoration,
                 child: Container(
                   margin:
                       const EdgeInsets.only(top: 3.0, left: 10.0, right: 10.0),
@@ -88,6 +96,9 @@ class Profile5 extends StatelessWidget {
                 ),
               ),
               Container(
+                decoration: makeControlDecoration(
+                  location: ProfileContainerLocation.rightmost,
+                ),
                 width: MediaQuery.of(context).size.width * 0.333,
                 height: MediaQuery.of(context).size.height * 0.6,
                 child: Column(
@@ -105,7 +116,6 @@ class Profile5 extends StatelessWidget {
                   ],
                 ),
                 alignment: Alignment(0.0, 0.0),
-                decoration: profilePartControlDecoration,
               ),
             ],
           ),

lib/profiles/profile_parts.dart
@@ -22,20 +22,55 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:crdi_mobile_app/route_settings.dart';
 
+enum ProfileContainerLocation {
+  leftmost,
+  inner,
+  rightmost,
+}
+
+BoxDecoration makeNonControlDecoration(
+    {@required ProfileContainerLocation location}) {
+  return BoxDecoration(
+    border: Border(
+      top: BorderSide(color: Colors.white30),
+      left: BorderSide(color: Colors.white30),
+      bottom: BorderSide(color: Colors.white30),
+      // FIXME: For value for false in following ternary:
+      // Fall back on Border's default value for 'right',
+      // instead of hard-coding its current default value.
+      right: (location == ProfileContainerLocation.rightmost)
+          ? BorderSide(color: Colors.white30)
+          : BorderSide.none,
+    ),
+  );
+}
+
 final BoxDecoration profilePartNonControlDecoration = BoxDecoration(
-  color: Color(0x00000000),
-  border: Border(
-    top: BorderSide(width: 1.0, color: Colors.white30),
-    left: BorderSide(width: 1.0, color: Colors.white30),
-    right: BorderSide(width: 1.0, color: Colors.white30),
-    bottom: BorderSide(width: 1.0, color: Colors.white30),
-  ),
+  border: Border.all(color: Colors.white30),
 );
 
+BoxDecoration makeControlDecoration(
+    {@required ProfileContainerLocation location}) {
+  return BoxDecoration(
+    border: Border(
+      left: BorderSide(
+        color: (location == ProfileContainerLocation.leftmost)
+            ? Colors.white30
+            : Colors.black,
+      ),
+      right: BorderSide(
+        color: (location == ProfileContainerLocation.rightmost)
+            ? Colors.white30
+            : Colors.black,
+      ),
+      bottom: BorderSide(color: Colors.black),
+    ),
+  );
+}
+
 final BoxDecoration profilePartControlDecoration = BoxDecoration(
-  color: Color(0x00000000),
   border: Border(
-    //top: BorderSide(width: 1.0, color: Colors.black),
+    top: BorderSide(width: 1.0, color: Colors.black),
     left: BorderSide(width: 1.0, color: Colors.white30),
     right: BorderSide(width: 1.0, color: Colors.black),
     bottom: BorderSide(width: 1.0, color: Colors.black),