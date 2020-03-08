aeba1343729c20cd566617a8c270070437d53662
Cole Howard <zizofifo@tutanota.com>
2020-03-07 23:22:44 -0600

    profile3.dart: Minor code style changes
    One of which came from dartfmt.

--- lib/profiles/profile3.dart
+++ lib/profiles/profile3.dart
@@ -127,7 +127,7 @@ class Profile3 extends StatelessWidget {
                       ],
                     ),
                   ),
-                )
+                ),
               ],
             ),
           ),
