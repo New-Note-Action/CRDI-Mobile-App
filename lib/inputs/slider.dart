ec54dade882491d6fcf4d8062812c50149a388b1
Cole Howard <zizofifo@tutanota.com>
2020-03-11 19:40:16 -0500

    Remove Expanded from SliderExampleState

--- lib/inputs/slider.dart
+++ lib/inputs/slider.dart
@@ -4,27 +4,24 @@ class SliderExampleState extends State {
   int _value = 1;
   @override
   Widget build(BuildContext context) {
-    return
-      new Expanded(
-          child: Slider(
-              value: _value.toDouble(),
-              min: 0.0,
-              max: 100.0,
-              divisions: 100,
-              activeColor: Colors.red,
-              inactiveColor: Colors.black,
-              //label: _value.toString(),
-              onChanged: (double newValue) {
-                setState(() {
-                  _value = newValue.round();
-                  print(_value);
-                });
-              },
-              semanticFormatterCallback: (double newValue) {
-                return '${newValue.round()} dollars';
-              }
-          )
-      );
+    return new Slider(
+      value: _value.toDouble(),
+      min: 0.0,
+      max: 100.0,
+      divisions: 100,
+      activeColor: Colors.red,
+      inactiveColor: Colors.black,
+      //label: _value.toString(),
+      onChanged: (double newValue) {
+        setState(() {
+          _value = newValue.round();
+          print(_value);
+        });
+      },
+      semanticFormatterCallback: (double newValue) {
+        return '${newValue.round()} dollars';
+      },
+    );
   }
 }
 
