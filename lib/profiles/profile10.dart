10da5051af831218f3ed9b53a9c8ae7635fb3400
Cole Howard <zizofifo@tutanota.com>
2020-04-08 14:43:44 -0500

    Add license/copyright notice and file descriptions to profiles
    File descriptions are the input layouts as presented on a sheet of paper
    provided by client. (This is also the same sheet of paper that describes the
    range of values the inputs should return.) The quote below is the legend from
    that sheet of paper:
    
    "Vertical Slide = VS
    "Horizontal Slide = HS
    "Button = B (continuous)
    "Toggle = T (static = on/off)
    "Joystick = J (continuous)"
    
    (Should the copyright year for the newer profiles be 2020 because those files
    did not exist until then, or should it be 2019-2020 as it is in this commit
    because they contain code from 2019?)

--- lib/profiles/profile10.dart
+++ lib/profiles/profile10.dart
@@ -1,3 +1,23 @@
+/* profile10.dart - VS + B */
+
+/*
+    Copyright (C) 2019-2020 "New Note Action" (John Elik, Cole Howard,
+    Sean Pohl, Tyler Tichler)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:crdi_mobile_app/inputs/button.dart';
